574315e0a72ad3b6e3fbade80cc4db3dfd3b5f18
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-03-16 20:20:30 -0400

    Made everything print on the same line

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import random
 from termcolor import colored
 
-# Get the words
-words = open("words.txt", "r")
-word_list = list((words.read()).split("\n"))
-library = {}
-for word in word_list:
-    library[word] = 1
+# Get the answer words
+possible_words = open("data/possible_words.txt", "r")
+possible_word_list = list((possible_words.read()).split("\n"))
 
-word_list_len = len(word_list)
+# Get the guess words
+allowed_words = open("data/allowed_words.txt", "r")
+allowed_word_list = list((allowed_words.read()).split("\n"))
+
+pos_list_len = len(possible_word_list)
 
 def word():
-    word_num = random.randint(1, word_list_len - 1)
-    word = word_list[word_num]
+    word_num = random.randint(1, pos_list_len - 1)
+    word = possible_word_list[word_num]
     return word
 
 def new_guess():
     guess = input(colored("Type a valid 5 letter word: ", "blue"))
 
-    # Make sure it is a valid guess
-    while guess not in library or len(guess) != 5:
+    while guess not in allowed_word_list or len(guess) != 5:
         print(colored("Sorry, that's invalid; Word must be valid and 5 characters", "red"))
         new_guess()
-    
+
     return guess
 
 guess = ""
@@ -41,11 +41,19 @@ while guess != ans and guess_number < 6:
     else:
         for c in range(5):
             if guess[c] == ans[c]:
-                print(colored(guess[c], "green"))
+                color = "green"
             elif guess[c] in ans:
-                print(colored(guess[c], "yellow"))
+                color = "yellow"
             else:
-                print(colored(guess[c], "dark_grey"))
-        
+                color = "dark_grey"
+
+            # Printing each character on the same line
+            if c == 4:
+                ending = "\n"
+            else:
+                ending = ""
+
+            print(colored(guess[c], color), end = ending)
+            
 # Loss if the player does not guess it in 6 attempts
 print(colored("Sorry, the word was", "blue"), colored(ans, "red"))
